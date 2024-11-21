e2e31e72e1bc7d96d51946b48d4437f9dcd8d773
alhanoufALLUHAYDDAN <alhanoufkhalaf1@gmail.com>
2024-11-21 08:49:10 +0300

    added models Car and Color to models.py

--- ShowRoom/brands/models.py
+++ ShowRoom/brands/models.py
@@ -5,25 +5,11 @@ from django.db import models
 
 class Brand(models.Model):
 
-    class Segment(models.TextChoices):
-        
-        ECONOMY = "economy" , "Economy"
-        LUXURY = "luxury" , "Luxury"
-        SPORTS = "sports" , "Sports"
-        ELECTRIC = "electric" , "Electric"
-        SEDAN = "sedan" , "Sedan"
-        WAGON = 'wagon', 'Wagon'
-        PREMIUM = 'premium', 'Premium'
-        SUV = 'suv', 'Sport Utility Vehicle' 
-        MINIVAN = 'minivan', 'Minivan'
-
-
     name = models.CharField(max_length=255)
     logo = models.ImageField(upload_to="images/")
     about = models.TextField()
     founded_at = models.DateField()
     origin = models.CharField(max_length=255)
-    segment = models.CharField(max_length=64, choices=Segment.choices)
     
     def __str__(self): return self.name
     
--- ShowRoom/cars/models.py
+++ ShowRoom/cars/models.py
@@ -1,5 +1,35 @@
 from django.db import models
+from brands.models import Brand
+from colorfield.fields import ColorField
 
 # Create your models here.
 
+class Color(models.Model):
+
+    name = models.CharField(max_length = 255)
+    hexadeicml_color = ColorField()
+    
+class Car(models.Model):
+
+    class Segment(models.TextChoices):
+        
+        ECONOMY = "economy" , "Economy"
+        LUXURY = "luxury" , "Luxury"
+        SPORTS = "sports" , "Sports"
+        ELECTRIC = "electric" , "Electric"
+        SEDAN = "sedan" , "Sedan"
+        WAGON = 'wagon', 'Wagon'
+        PREMIUM = 'premium', 'Premium'
+        SUV = 'suv', 'Sport Utility Vehicle' 
+        MINIVAN = 'minivan', 'Minivan'
+
+    name = models.CharField(max_length=255)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="images/")
+    specs = models.TextField()
+    model = models.CharField(max_length=255)
+    segment = models.CharField(max_length=64, choices=Segment.choices)
+    color = models.ManyToManyField(Color)
+
+    def __str__(self): return self.name
 
