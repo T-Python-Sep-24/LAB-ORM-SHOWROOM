a323509ac198bc141e4e43f7bccf546a6cd28f45
alhanoufALLUHAYDDAN <alhanoufkhalaf1@gmail.com>
2024-11-21 17:38:20 +0300

    finished base.html, started home page, and added new attributes to car model

--- ShowRoom/cars/models.py
+++ ShowRoom/cars/models.py
@@ -30,6 +30,8 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     segment = models.CharField(max_length=64, choices=Segment.choices)
     color = models.ManyToManyField(Color)
+    price = models.DecimalField(max_digits=15,decimal_places=10, default=0.0)
+    currency = models.CharField(max_length=3, default='SAR')
 
     def __str__(self): return self.name
 
