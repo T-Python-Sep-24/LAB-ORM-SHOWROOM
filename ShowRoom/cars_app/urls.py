6072ce70f32bc1bec7dba4a3e5e07c57f66b6ba8
Reema <reemaalbrahim28@gmail.com>
2024-11-22 13:53:41 +0300

    Adding brands and colors

--- ShowRoom/cars_app/urls.py
+++ ShowRoom/cars_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('new/', views.new_car_view, name='new_car_view'), 
     path('update/<car_id>/', views.update_car_view, name='update_car_view'), 
     path('colors/new/', views.new_color_view, name='new_color_view'), 
+    path('colors/all/', views.all_colors_view, name='all_colors_view'), 
     path('colors/update/<color_id>/', views.update_color_view, name='update_color_view'), 
 
 
--- ShowRoom/cars_app/views.py
+++ ShowRoom/cars_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Brand, Color  # Import your models
 
 # Create your views here.
 def all_cars_view(request):
@@ -10,20 +11,38 @@ def car_detail_view(request):
     return render(request, 'cars_app/car_detail.html', )
 
 def new_car_view(request):
-       
-    return render(request, 'cars_app/new_car.html')
+    brands = Brand.objects.all()  # Fetch all brands
+    colors = Color.objects.all()   # Fetch all colors
+
+    return render(request, 'cars_app/new_car.html', {
+        'brands': brands,
+        'colors': colors
+    })
 
 def update_car_view(request):
       
     return render(request, 'cars_app/update_car.html')
 
 def new_color_view(request):
-      
-    return render(request, 'cars_app/new_color.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        hex_code = request.POST.get('hex_code')
+        
+        # Create a new Color instance
+        Color.objects.create(name=name, hex_code=hex_code)
+        
+        # Redirect to the all colors view
+        return redirect('cars_app:all_colors_view')  # Adjust the URL name as necessary
 
+    return render(request, 'cars_app/new_color.html')
+      
 def update_color_view(request):
 
     return render(request, 'cars_app/update_color.html')
 
+
+def all_colors_view(request):
+    colors = Color.objects.all()  # Fetch all colors from the database
+    return render(request, 'cars_app/all_colors.html', {'colors': colors})
 
 
