2da975451d0e2e3b042152dc8c72b1b0d35e3cb7
AR <aarokan@gmail.com>
2024-11-24 00:11:11 +0300

    Create Brand, Color and Car models

--- ShowRoom/brands/models.py
+++ ShowRoom/brands/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=256)
+    logo = models.ImageField(upload_to="images/", default="images/default.jpg")
+    about = models.TextField()
+    founded_at = models.DateField()
+
+    def __str__(self):
+        return self.name
--- ShowRoom/cars/models.py
+++ ShowRoom/cars/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from brands.models import Brand
 
 # Create your models here.
+class Color(models.Model):
+    name = models.CharField(max_length=256)
+    hexadecimal = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+    
+
+class Car(models.Model):
+    name = models.CharField(max_length=256)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, null=True)
+    colors = models.ManyToManyField(Color)
+    photo = models.ImageField(upload_to="images/", default="images/default.jpg")
+    specs = models.TextField()
+    model = models.IntegerField()
+
+    def __str__(self):
+        return self.name
