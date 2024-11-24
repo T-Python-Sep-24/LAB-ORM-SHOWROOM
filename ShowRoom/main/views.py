b796039d18ae9bf2bd8ad323c584c5636ce16e01
AR <aarokan@gmail.com>
2024-11-24 04:10:04 +0300

    Create cars list component

--- ShowRoom/main/views.py
+++ ShowRoom/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from cars.models import Car
 
 # Create your views here.
 def home_view(request: HttpRequest):
+    cars = Car.objects.all()
+    context = {"cars": cars}
 
-    return render(request, "main/index.html")
+    return render(request, "main/index.html", context)
