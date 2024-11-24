d2b871c2e6f75af0fbf0a27e196697c2cb97a8c7
AR <aarokan@gmail.com>
2024-11-24 04:55:25 +0300

    Show latest 4 cars in the home page

--- ShowRoom/main/views.py
+++ ShowRoom/main/views.py
@@ -4,7 +4,7 @@ from cars.models import Car
 
 # Create your views here.
 def home_view(request: HttpRequest):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by("-id")[0:4]
     context = {"cars": cars}
 
     return render(request, "main/index.html", context)
