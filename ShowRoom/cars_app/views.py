7a665b5c8dd14913652743c24054ab274785a369
Reema <reemaalbrahim28@gmail.com>
2024-11-25 11:36:40 +0300

    Make migrations and add cars successfuly

--- ShowRoom/cars_app/views.py
+++ ShowRoom/cars_app/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, redirect
-from .models import Brand, Color, Car  # Import your models
-#from django.db import IntegrityError
-from .forms import CarForm  # Import the CarForm
-from django.contrib import messages
-from django.http import HttpRequest
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Color, Car  # Import your models
+from brands_app.models import Brand 
+from django.db import IntegrityError
+from django.http import HttpRequest, HttpResponse
 
 def all_cars_view(request):
     cars = Car.objects.all()
@@ -15,27 +14,48 @@ def car_detail_view(request, car_id):
     car = Car.objects.get(id=car_id)  # Fetch the specific car based on ID
     return render(request, 'cars_app/car_detail.html', {'car': car})
 
-def new_car_view(request: HttpRequest):
-    # Fetch all brands and colors to display in the form
+def new_car_view(request):
+    if request.method == 'POST':
+        print("POST request received.")
+        
+        name = request.POST.get('name')
+        brand_id = request.POST.get('brand')  # Ensure the name matches the form input
+        colors_ids = request.POST.getlist('colors')  # Ensure the name matches the form input
+        photo = request.FILES.get('photo')
+        specs = request.POST.get('specs')
+        model = request.POST.get('model')
+
+        brand = get_object_or_404(Brand, id=brand_id)
+        print(brand.id)
+        print(colors_ids)
+
+
+        # Create the new Car instance
+        car = Car(
+            name=name,  
+            brand=brand, 
+            photo=photo, 
+            specs=specs, 
+            model=model
+        )
+
+        car.save()
+
+        # Add selected colors to the car
+        print(colors_ids)
+        for color_id in colors_ids:
+            color = Color.objects.get(pk=color_id)
+            car.colors.add(color)
+
+        return redirect('cars_app:all_cars_view')  # Redirect to the all cars view
+
+    # Fetch brands and colors for the form if it's a GET request
     brands = Brand.objects.all()
     colors = Color.objects.all()
-
-    if request.method == 'POST':
-        form = CarForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Car added successfully!")
-            return redirect('cars_app:all_cars_view')
-        else:
-            messages.error(request, "Please correct the errors below.")
-
-    else:
-        form = CarForm()  # Create a new instance of the form for GET requests
-
+    
     return render(request, 'cars_app/new_car.html', {
-        'form': form,
-        'brands': brands,  # Pass brands to the template
-        'colors': colors,  # Pass colors to the template
+        'brands': brands,
+        'colors': colors,
     })
 
 
